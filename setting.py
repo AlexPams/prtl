5a43594bb0f11ac269db1cae818ddcd9d766a50b
Aleksandr Vetrov <98459125+AlexPams@users.noreply.github.com>
2022-04-19 08:53:32 +0000

    Application calculator
    Need to add new function button like clear, sqrt, pow, etc. Other fucntion is done. Calculator ready for use

--- setting.py
+++ setting.py
@@ -1,4 +1,4 @@
-SINGS = ['÷', 'x', '-', '+']
+SINGS = ['/', '*', '-', '+']
 FIRST_ROW = [7,8,9]
 SECOND_ROW = [4,5,6]
 THIRD_ROW = [1,2,3]
--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,16 +1,19 @@
-const output = document.getElementById('out')
-
-const sings = document.querySelectorAll('.sing')
-const digits = document.querySelectorAll('.digit')
+const output = document.getElementById('out');
+const sings = document.querySelectorAll('.sing');
+const digits = document.querySelectorAll('.digit');
 
 digits.forEach(button => {
     button.addEventListener('click', function () {
-        if(output.value ==" ") {
-            output.value = button.value;
-        }else if(button.value == 'C'){
-            output.value = "";
-        } else{
-           output.value += button.value;
+            output.value += button.value;
+    })
+})
+
+sings.forEach(button => {
+    button.addEventListener('click', function () {
+        if (button.value == "=") {
+            output.value = eval(output.value)
+        } else {
+            output.value += ` ${button.value} `;
         }
     })
 })
